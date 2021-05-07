121c86413ac0a5d11998b06c1a2e59ce2896f69b
Wenter <uuooooq@qq.com>
2021-05-07 15:25:21 +0800

    测试读写文件

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,13 @@
 import React from 'react'
 
 import '../assets/css/App.css'
-const fs = window.require('fs').promises;
-
-const fileHelper = {
-  readFile: (path) => {
-    return fs.readFile(path, {encoding: 'utf8'})
-  },
-  writeFile: (path, content) => {
-    return fs.writeFile(path, content,{encoding: 'utf8'})
-  },
-  renameFile: (path, newPath) => {
-    return fs.rename(path, newPath)
-  },
-  deleteFile: (path) => {
-    return fs.unlink(path)
-  },
-}
+import fileHelper from './tool/fileHelper';
 
 function App() {
   const [value,setValue] = React.useState('xx');
 
   React.useEffect(()=>{
-    fileHelper.readFile('/Users/zhudongwei/Documents/sfjsldfj.md').then(value => {
+    fileHelper.readFile('/Users/zhudongwei/Documents/menaljs.chm').then(value => {
       setValue(value);
     })
   })
